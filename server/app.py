fbfc2b72500df425f3e32b7f4901b6abf2b7b5a7
Mark Beale <bealebeaverst@gmail.com>
2024-09-17 20:53:25 -0700

    Complete Lab

--- server/app.py
+++ server/app.py
@@ -18,7 +18,6 @@ api = Api(app)
 
 
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
@@ -30,23 +29,42 @@ class Plants(Resource):
             name=data['name'],
             image=data['image'],
             price=data['price'],
+            is_in_stock=data.get('is_in_stock', True)
         )
 
         db.session.add(new_plant)
         db.session.commit()
 
-        return make_response(new_plant.to_dict(), 201)
+        return make_response(jsonify(new_plant.to_dict()), 201)
 
 
 api.add_resource(Plants, '/plants')
 
 
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        return make_response(jsonify({"error": "Plant not found"}), 404)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            data = request.get_json()
+            for attr in data:
+                setattr(plant, attr, data[attr])
+            db.session.commit()
+            return make_response(jsonify(plant.to_dict()), 200)
+        return make_response(jsonify({"error": "Plant not found"}), 404)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response('', 204)
+        return make_response(jsonify({"error": "Plant not found"}), 404)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
